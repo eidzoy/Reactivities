d0bb0986663ae52d66b2ac39987771c004836faf
user <elieeid-02@hotmail.com>
2023-07-03 16:41:08 +0300

    end of sec2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Controller]
+    [Route("api/[Controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
